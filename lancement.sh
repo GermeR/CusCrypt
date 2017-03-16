e7a26c1fcd2aef10df7927ccd71d8e937642a58d
Robin Germe <robin.germe@etudiant.univ-lille1.fr>
2017-03-16 21:10:12 +0100

    je t'envoi le rendu par mail

--- lancement.sh
+++ lancement.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+tput setaf 2
+
 cd ./webapps/CusCrypt 2>/dev/null && echo Chemin OK || echo $(basename $(pwd)) | grep CusCrypt 1>/dev/null || echo Déplacez vous dans tomcat/webapps/CusCrypt
 
-#./comp.sh > /dev/null && echo Compilation OK
+./comp.sh > /dev/null && echo Compilation OK
 
+tput sgr0
 
+../../bin/catalina.sh run 1>catalina.ok.log 2>catalina.err.log &
 
+tput setaf 2
 iceweasel "http://localhost:8080/CusCrypt/new.html" 1>/dev/null 2>/dev/null || chromium-browser "http://localhost:8080/CusCrypt/new.html" || echo Veuillez cliquer sur "http://localhost:8080/CusCrypt/new.html"
 
 echo "Appuyer sur une touche pour continuer"
 echo
+
 read c
 
 echo "pour vérifier la base de données pendant le déroulement de vos tests vous devez disposer de sqlite3"
@@ -30,3 +36,5 @@ read c
 echo
 echo "pour tester notre programme nous vous proposons de créer differents utilisateurs \net d'observer l'évolution de la base de mots de passes avec la commande sqlite3 (disponibles sur les dépots debian) ci-dessus"
 echo
+
+tput sgr0
